fb1a2c526f76e3a63fd9f40b80d6292149b6da09
Rolando15 <rolando.hernandez@bryanuniversity.edu>
2022-06-15 16:36:12 -0400

    test

--- Coffee_shop/index.js
+++ Coffee_shop/index.js
@@ -1,47 +1,50 @@
+function myForm(); {
+let x = document.getElementsByClassName(value)[2];
+var defaultVal = x.defaultValue;
+var currentVal = x.value;
 
+if (defaultVal == currentVal) {
+  document.getElementById(x).innerHTML = "Current value is : "
+  + x.defaultValue + " and " + x.value
+  + "Change the number of number field to see the difference!";
+} 
+}
+myForm();
 
-let a = document.body.getElementsByClass("div1")[7]
-console.log(a)
-
-
-
-let b = input()
-
-
-function myAddition(numm1, numm2){
-    return  Number(numm1) + Number(numm2);
+  // function myAddition(numm1, numm2){
+//     return  Number(numm1) + Number(numm2);
     
-}
+// }
 
 
 
 
 
-function mySubtraction(numm1, numm2){
-    return Number(numm1) - Number(numm2);
+// function mySubtraction(numm1, numm2){
+//     return Number(numm1) - Number(numm2);
 
-}
-
-
+// }
 
 
 
 
 
 
-document.getElementById("myNumber").addEventListener("click", function(){
-    var firstnum = document.getElementById("firstNumber").value
-     var secondnum = document.getElementById("my2ndNumber").value
-    document.getElementById("add").textContent = "The result is " + myAddition (firstnum, secondnum)
+
+
+// document.getElementById("myNumber").addEventListener("click", function(){
+//     var firstnum = document.getElementById("firstNumber").value
+//      var secondnum = document.getElementById("my2ndNumber").value
+//     document.getElementById("add").textContent = "The result is " + myAddition (firstnum, secondnum)
  
- });
+//  });
 
 
 
- document.getElementById("subtractButton").addEventListener("click", function(){
-    var thirdnum =  document.getElementById("thirdNumber").value
-    var fourthnum = document.getElementById("fourthNumber").value
-   document.getElementById("subtract").textContent = "The result is " + mySubtraction (thirdnum, fourthnum)
+//  document.getElementById("subtractButton").addEventListener("click", function(){
+//     var thirdnum =  document.getElementById("thirdNumber").value
+//     var fourthnum = document.getElementById("fourthNumber").value
+//    document.getElementById("subtract").textContent = "The result is " + mySubtraction (thirdnum, fourthnum)
 
-});
+// });
  
